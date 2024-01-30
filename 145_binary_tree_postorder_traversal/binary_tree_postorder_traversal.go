489de84e42fada1e06a836b59f6abe7389bb0c90
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2024-01-30 23:38:30 +0600

    solution: (easy) Binary Tree Postorder Traversal

---
+++ 145_binary_tree_postorder_traversal/binary_tree_postorder_traversal.go
@@ -0,0 +1,30 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	result := []int{}
+	stack := []*TreeNode{root}
+
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		result = append([]int{node.Val}, result...)
+
+		if node.Left != nil {
+			stack = append(stack, node.Left)
+		}
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+		}
+	}
+
+	return result
+}
