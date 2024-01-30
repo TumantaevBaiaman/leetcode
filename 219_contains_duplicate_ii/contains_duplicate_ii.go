fe476a43350e92597c7f0e89b72b3ff595a00b26
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2024-01-30 21:53:02 +0600

    solution: (easy) Contains Duplicate II

---
+++ 219_contains_duplicate_ii/contains_duplicate_ii.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	numIndex := make(map[int]int)
+	for i, num := range nums {
+		if index, ok := numIndex[num]; ok && i-index <= k {
+			return true
+		}
+		numIndex[num] = i
+	}
+	return false
+}
