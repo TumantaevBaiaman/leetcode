b018ef36c7f136d706b939cc7cb8e364cb8bb93b
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2024-02-03 21:07:28 +0600

    solution: (medium) jump game

---
+++ 55_jump-game/jump-game.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func canJump(nums []int) bool {
+	maxReach := 0
+	for i := 0; i < len(nums); i++ {
+		if i > maxReach {
+			return false
+		}
+		maxReach = max(maxReach, i+nums[i])
+		if maxReach >= len(nums)-1 {
+			return true
+		}
+	}
+	return false
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
