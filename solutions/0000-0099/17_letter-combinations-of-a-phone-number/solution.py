e5e8b68fbe598db611468d6d14f17669f6a9faaa
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2025-04-29 00:48:42 +0600

    fix: 17 - python (medium)

--- solutions/0000-0099/17_letter-combinations-of-a-phone-number/solution.py
+++ solutions/0000-0099/17_letter-combinations-of-a-phone-number/solution.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def letterCombinations(self, digits):
         if not digits:
-            return digits
+            return []
 
         phone_map = {
             '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
@@ -20,5 +20,5 @@ class Solution(object):
                 backtrack(index + 1, path)
                 path.pop()
 
-    backtrack(0, [])
-    return res
+        backtrack(0, [])
+        return res
