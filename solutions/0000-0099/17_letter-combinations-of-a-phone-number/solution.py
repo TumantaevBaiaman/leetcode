83d89c9772abae4b22945cdc6be791669a99551a
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2025-04-29 00:44:36 +0600

    solution: 17 - python (medium)

---
+++ solutions/0000-0099/17_letter-combinations-of-a-phone-number/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        if not digits:
+            return digits
+
+        phone_map = {
+            '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+            '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
+        }
+
+        res = []
+
+        def backtrack(index, path):
+            if index == len(digits):
+                res.append(''.join(path))
+                return
+            possible_letters = phone_map[digits[index]]
+            for letter in possible_letters:
+                path.append(letter)
+                backtrack(index + 1, path)
+                path.pop()
+
+    backtrack(0, [])
+    return result
